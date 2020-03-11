b23c5736bc1a2a0928d2500ac8d3c47bde746c7d
Mike Jackson <mike@think-brew.com>
2020-03-11 08:36:54 -0500

    Add About Me template

---
+++ wp-content/themes/FrontEndMike-2019/template-about-me.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Template Name: About Me template
+*/
+
+get_header(); ?>
+			
+	<div class="content ">
+		<div class="blog-hero">
+			<div class="row">
+				<div class="">
+					<h3><?php the_title(); ?></h3>
+				</div>
+			</div>
+		</div>
+
+
+	
+		<div class="inner-content full-width">
+			<div class="row">
+				<div class="large-12 medium-12 small-12 columns">
+					<p>
+						Hi, I'm Michael Jackson, a Front End Web Developer from Baton Rouge, La. I have over 3 years of professional experience as a web developer. Web development is truly my passion. I enjoy using code in order to help businesses achieve their goals by creating beautiful responsive websites.
+					</p>
+					<p>
+						When I'm not working on websites I like to involve myself in the developer community in other ways. First, I along with fellow developer <a class="link" href="https://twitter.com/quintonjasonjr" target="_blank" rel="noopener noreferrer">Quinton Jason</a> created <a class="link" href="https://www.latechpark.com/events/2017/8/30/br-hack-night" target="_blank" rel="noopener noreferrer">BR HackNight</a>. A meet-up group for local web developers to gather and talk shop. Secondly, I am a Lead Instructor with <a class="link" href="https://www.thefuturesfund.org/" target="_blank" rel="noopener noreferrer">Futures Fund</a>. An organization created through the Walls Project to teach middle and high school students coding and photography.
+					</p>
+					<p>
+						My goal as a developer is to enrich the lives of those I work with by using code to create solutions. Whether it be eCommerce, Education, or local businesses everyone needs a website these days. The way I can contribute to that is, I can build the website as well as assist in populating it with content (through blogging or copy) and create documentation so that the client can maintain the site on their own if they so wish.
+					</p>
+					<p>
+						I have been fortunate to work with some great people and companies in my time as a developer and if you would like to work with me to help to increase your web presence please <a class="link" href="mailto:michael.lenard.jackson@gmail.com">Contact Me</a>
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="large-4 medium-6 small-12 columns">
+					<img src="https://frontendmike.com/wp-content/uploads/2019/05/FrontEndMike.jpg" alt="">
+				</div>
+				<div class="large-4 medium-6 small-12 columns">
+					<img src="https://frontendmike.com/wp-content/uploads/2019/05/hacknight_group.jpg" alt="">
+				</div>
+				<div class="large-4 medium-6 small-12 columns">
+					<img class="alignnone size-medium wp-image-113" src="https://frontendmike.com/wp-content/uploads/2019/07/BRhacknight-300x300.jpg" alt="" width="300" height="300" />
+				</div>
+			</div>
+			
+	
+		    
+
+		    
+		</div> <!-- end #inner-content -->
+	
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
