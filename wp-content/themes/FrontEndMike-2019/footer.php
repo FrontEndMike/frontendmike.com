abb15f115b8d83f0b89a4eca7ce144538a4ff1a7
Mike Jackson <mike@think-brew.com>
2020-05-20 18:10:31 -0500

    Remove footer widgets

--- wp-content/themes/FrontEndMike-2019/footer.php
+++ wp-content/themes/FrontEndMike-2019/footer.php
@@ -11,24 +11,9 @@
 				<footer class="footer" role="contentinfo">
 					
 					<div class="row">
-	    				<div id="footer-widget" class="small-6 medium-6 large-6 column" role="complementary">
-
-	    					<?php if ( is_active_sidebar( 'footer' ) ) : ?>
-
-	    						<?php dynamic_sidebar( 'footer' ); ?>
-
-	    					<?php endif; ?>
-
-	    				</div>
-						<div class="small-6 medium-6 large-6 column">
-							<nav role="navigation">
-	    						<?php joints_footer_links(); ?>
-	    					</nav>
-	    				</div>
-						<div class="row">
+	    				
 							<div class="small-12 medium-12 large-12 column">
 								<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
-							</div>
 						</div>
 					
 					</div> <!-- end #inner-footer -->
