d23430ab2b2d0f45bf098f0c1602ee48acf92cad
Mike <michael.lenard.jackson@gmail.com>
2020-05-24 16:17:15 -0500

    Final design of projects page

--- wp-content/themes/FrontEndMike-2019/archive-projects.php
+++ wp-content/themes/FrontEndMike-2019/archive-projects.php
@@ -14,11 +14,23 @@ get_header(); ?>
 	</div>
 			
 	<div class="content">
-	
+	<div class="row">
+		<div class="large-12 columns">
+			<div id="category" class="filter-button-group">
+				<button class="cat-btn active-cat" data-filter="*">Show All</button>
+				<button class="cat-btn" data-filter=".wordpress">WordPress</button>
+				<button class="cat-btn" data-filter=".foundation">Foundation</button>
+				<button class="cat-btn" data-filter=".javascript">JavaScript</button>
+				<button class="cat-btn" data-filter=".bootstrap">Bootstrap</button>
+				<!-- <button data-filter=".metal:not(.transition)">metal but not transition</button> -->
+			</div>
+		</div>
+	</div>
+
 		<div class="inner-content row">
 
-			<div class="project-grid">
-				<div class="col">
+			<div class="project-grid grid">
+				<div class="col" data-equalizer>
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				
 						<!-- To see additional archive styles, visit the /parts directory -->
@@ -39,5 +51,33 @@ get_header(); ?>
 		</div> <!-- end #inner-content -->
 	    
 	</div> <!-- end #content -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+ 	<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+	<script>
+ 	var $grid = $('.grid').isotope({
+ 	  // options
+ 	  itemSelector: '.grid-item',
+ 	  layoutMode: 'fitRows'
+ 	});
+
+ 	// filter items on button click
+ 	$('.filter-button-group').on( 'click', 'button', function() {
+ 	  var filterValue = $(this).attr('data-filter');
+ 	  $grid.isotope({ filter: filterValue });
+ 	});
+
+ 	// Add active class to the current button (highlight it)
+ 	var header = document.getElementById("category");
+ 	var btns = header.getElementsByClassName("cat-btn");
+ 	for (var i = 0; i < btns.length; i++) {
+ 	  btns[i].addEventListener("click", function() {
+ 	    var current = document.getElementsByClassName("active-cat");
+ 	    current[0].className = current[0].className.replace(" active-cat", "");
+ 	    this.className += " active-cat";
+ 	  });
+ 	}
+
+
+ </script>
 
 <?php get_footer(); ?>
--- wp-content/themes/FrontEndMike-2019/parts/project-archive-grid.php
+++ wp-content/themes/FrontEndMike-2019/parts/project-archive-grid.php
@@ -16,22 +16,22 @@ $grid_columns = 4; ?>
 	<?php endif; ?>
 
 
-			<!--Item: -->
-			
-				<div class="small-12 medium-6 large-4 columns panel <?php $my_terms = get_the_terms( $post->ID, 'custom_cat' ); 
-														if( $my_terms && !is_wp_error( $my_terms ) ) { 
-														foreach( $my_terms as $term ) { 
-																echo $term->slug;
-																echo ' ';
-															 } 
-													}?>" data-equalizer-watch>
-				<a href="<?php the_permalink() ?>" rel="bookmark" alt="<?php the_title_attribute(); ?>">
-					<div class="project-panel">
-						<?php the_post_thumbnail('full'); ?>
-						<h2 class="text-center"><?php the_title(); ?></h2>
-					</div>
-				</a>
+		<!--Item: -->
+		
+			<div class="small-12 medium-6 large-4 columns panel grid-item <?php $my_terms = get_the_terms( $post->ID, 'custom_cat' ); 
+													if( $my_terms && !is_wp_error( $my_terms ) ) { 
+													foreach( $my_terms as $term ) { 
+															echo $term->slug;
+															echo ' ';
+															} 
+												}?>" data-equalizer-watch>
+			<a href="<?php the_permalink() ?>" rel="bookmark" alt="<?php the_title_attribute(); ?>">
+				<div class="project-panel">
+					<?php the_post_thumbnail('full'); ?>
+					<h2 class="text-center"><?php the_title(); ?></h2>
 				</div>
+			</a>
+			</div>
 			
 
 	<?php if( 0 === ( $wp_query->current_post + 1 )  % $grid_columns ||  ( $wp_query->current_post + 1 ) ===  $wp_query->post_count ): ?>
