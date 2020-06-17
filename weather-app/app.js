8fe25c979583234480dc97006fa3dbb28e982021
Mike <michael.lenard.jackson@gmail.com>
2020-06-16 22:45:12 -0500

    Add functions to set user position, show error, and get weather from api

--- weather-app/app.js
+++ weather-app/app.js
@@ -25,3 +25,25 @@ if('geolocation' in navigator){
     notificationElement.style.display = "block";
     notificationElement.innerHTML = "<p> Browser doesnt support geolocation</p>";
 }
+
+
+//SET USER'S POSITION
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    getWeather(latitude, longitude);
+}
+
+// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
+function showError(error){
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<p>${error.message}</p>`
+}
+
+// GET WEATHER FROM API PROVIDER
+function getWeather(latitude, longitude){
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    console.log(api);
+}
