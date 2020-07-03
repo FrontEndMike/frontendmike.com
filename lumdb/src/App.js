6ca4c0a704af59d79c74c3fefe47e1081d184bff
Mike <michael.lenard.jackson@gmail.com>
2020-07-03 17:58:25 -0500

    State, Inline Conditionals & Basic Events

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -4,16 +4,31 @@ import './App.css';
 
 
 class App extends Component {
+
+  state = {
+    toggle: true
+  }
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" /> 
-          <Welcome text="Welcome to using Props" />
+          <Welcome text="Welcome to using Props" toggle={this.state.toggle} />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this.state.toggle &&
+          <p>This show show and hide</p>
+        }
+        <button onClick={this.toggle}> Show / Hide </button>
       </div>
     );
   }
@@ -21,7 +36,8 @@ class App extends Component {
 
 class Welcome extends Component {
   render() {
-    const { text } = this.props;
+    const { text, toggle } = this.props;
+    console.log(toggle);
     return(
       <h1 className="App-title">{text}</h1>
     )
