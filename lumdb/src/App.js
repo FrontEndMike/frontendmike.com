07e1d4702c6d009925a8affe864688878db417d7
Mike <michael.lenard.jackson@gmail.com>
2020-07-02 23:00:52 -0500

    Create first component

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -9,13 +9,22 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" /> 
+          <Welcome />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    return(
+      <h1 className="App-title">{welcome}</h1>
+    )
   }
 }
 
