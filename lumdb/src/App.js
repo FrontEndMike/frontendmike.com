f8a612f7baf833865486b235e97ee44595e759dd
Mike <michael.lenard.jackson@gmail.com>
2020-07-02 23:26:53 -0500

    intro to props

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const welcome = "Welcome to React";
 
 class App extends Component {
   render() {
@@ -10,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" /> 
-          <Welcome />
+          <Welcome text="Welcome to using Props" />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -22,8 +21,9 @@ class App extends Component {
 
 class Welcome extends Component {
   render() {
+    const { text } = this.props;
     return(
-      <h1 className="App-title">{welcome}</h1>
+      <h1 className="App-title">{text}</h1>
     )
   }
 }
