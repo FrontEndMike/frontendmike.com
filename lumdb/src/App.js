7c84226b6e32fffd54eba6534bf10a6841838601
Mike <michael.lenard.jackson@gmail.com>
2020-07-06 20:36:51 -0500

    Introduction to controlled and uncontrolled inputs

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -4,6 +4,15 @@ import './App.css';
 
 
 class App extends Component {
+  state = {
+    input: 'Hello'
+  }
+
+  updateInput = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  }
 
   submit = () => {
     console.log(this.text.value);
@@ -16,9 +25,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" /> 
           <Welcome text="Welcome to using Props" />
         </header>
+        <h3>{this.state.input}</h3>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* Controlled input */}
+        <input type="text" onChange={this.updateInput} value={this.state.input} />
+        {/* Uncontrolled input */}
         <input type="text" ref={(input) => this.text  = input} />
         <button onClick={this.submit}> Show Value </button>
       </div>
