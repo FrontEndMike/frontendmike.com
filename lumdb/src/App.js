1d0bc4510e85a80a89de171b7e9cc3ae899f2122
Mike <michael.lenard.jackson@gmail.com>
2020-07-07 23:39:30 -0500

    remove old code

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -25,15 +25,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" /> 
           <Welcome text="Welcome to using Props" />
         </header>
-        <h3>{this.state.input}</h3>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* Controlled input */}
-        <input type="text" onChange={this.updateInput} value={this.state.input} />
-        {/* Uncontrolled input */}
-        <input type="text" ref={(input) => this.text  = input} />
-        <button onClick={this.submit}> Show Value </button>
+
       </div>
     );
   }
