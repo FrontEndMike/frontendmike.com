0bb9ecaae8b06ff68c649cbb116a5866c7b12dec
Mike <michael.lenard.jackson@gmail.com>
2020-07-08 23:48:35 -0500

    Add an array of movies to iterate

--- lumdb/src/App.js
+++ lumdb/src/App.js
@@ -2,42 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  },
+  {
+    id: 2,
+    title: 'Spider Man'
+  },
+  {
+    id: 3,
+    title: 'Along Came Polly'
+  }
+];
 
 class App extends Component {
-  state = {
-    input: 'Hello'
-  }
-
-  updateInput = (event) => {
-    this.setState({
-      input: event.target.value
-    })
-  }
-
-  submit = () => {
-    console.log(this.text.value);
-  }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" /> 
-          <Welcome text="Welcome to using Props" />
         </header>
+        {movies.map((movie) => {
+          return(
+            <div>
+                {movie.title}
+            </div>
+          )
+        })}
 
       </div>
     );
   }
 }
 
-class Welcome extends Component {
-  render() {
-    const { text } = this.props;
-    return(
-      <h1 className="App-title">{text}</h1>
-    )
-  }
-}
+
 
 export default App;
