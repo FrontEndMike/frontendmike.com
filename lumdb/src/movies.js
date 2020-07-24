e344625ef026e303fecf67496f66e25c455be4d2
Mike <michael.lenard.jackson@gmail.com>
2020-07-24 00:57:00 -0500

    Add movie poster path

--- lumdb/src/movies.js
+++ lumdb/src/movies.js
@@ -2,8 +2,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+
 const Movie = ({ movie }) => (
   <div>
+    <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
     <h3>{movie.title}</h3>
     <p>{movie.overview}</p>
   </div>
