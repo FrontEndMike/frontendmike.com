b408d9e0f544533cf654b9f91335f67902ca6ce4
Mike Jackson <michael.lenard.jackson@gmail.com>
2025-04-12 21:03:13 -0400

    update single project page

--- wp-content/themes/FrontEndMike-2025/template-parts/content-single.php
+++ wp-content/themes/FrontEndMike-2025/template-parts/content-single.php
@@ -1,16 +1,27 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div class="flex flex-wrap md:flex-nowrap gap-4 mb-8">
+	<div class="grid grid-cols-1 md:grid-cols-5 gap-4 mb-8">
 		<?php if (has_post_thumbnail()) : ?>
-		<div class="basis-full md:basis-1/2 lg:basis-2/5">
+		<div class="col-span-2">
 			<img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" 
 					alt="<?php the_title_attribute(); ?>" 
-					class="w-full h-48 object-cover rounded-t-lg">
+					class="w-full drop-shadow-xl object-cover rounded-2xl">
 			</div>
 			<?php endif; ?>
-		<div class="basis-full md:basis-1/2 lg:basis-3/5">
-			<h2 class="text-2xl sm:text-4xl font-extrabold leading-tight mb-1"><?php the_title(); ?></h2>
+		<div class="col-span-3">
+			<h1 class="text-2xl leading-[24px] sm:text-4xl font-extrabold sm:leading-[30px] mb-2"><?php the_title(); ?></h1>
 			<time datetime="<?php echo get_the_date( 'c' ); ?>" itemprop="datePublished" class="text-sm text-gray-700"><?php echo get_the_date(); ?></time>
+			<?php
+				$categories = get_the_terms( get_the_ID(), 'project_category' );
+				if ( $categories && ! is_wp_error( $categories ) ) :
+				?>
+					<p class="text-sm text-gray-500 block mt-2 mb-2">
+						<?php
+						$category_names = wp_list_pluck( $categories, 'name' );
+						echo implode( ' – ', $category_names );
+						?>
+					</p>
+				<?php endif; ?>
 
 			<div class="flex flex-wrap mt-4 gap-4">
 				<?php $github_link = get_field('github_link'); if ($github_link) : ?>
@@ -28,6 +39,7 @@
 						View Project
 					</a>
 				<?php endif; ?>
+
 			</div>
 		</div>
 	</div>
